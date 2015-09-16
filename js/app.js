0a5ddc556629d58bbd1c1b874351075536b4850a
segmentationfaulter <segmentationfaulter@gmail.com>
2015-09-16 16:25:59 +0500

    show the channel name and stream status

--- js/app.js
+++ js/app.js
@@ -25,13 +25,27 @@ $(document).ready(() => {
   // ajax request success callback
   function success(response) {
     let streamStatus;
+    let displayName;
     if (response.stream === null) {
       streamStatus = 'Offline';
     }
     else {
       streamStatus = 'Live';
     }
-    let html = `<li><span id="stream-status">${streamStatus}</span></li>`;
-    $('#channels').append(html);
+
+    // further send another ajax request to get other details about the channel
+    $.ajax({
+      url : response._links.channel,
+      type : 'GET',
+      dataType : 'json',
+      success : moreSuccess
+    });
+
+    function moreSuccess(resp) {
+      console.log(resp.display_name);
+      displayName = resp.display_name;
+      let html = `<li><span id="display-name">${displayName}</span>   <span id="stream-status">${streamStatus}</span></li>`;
+      $('#channels').append(html);
+    }
   }
 });
