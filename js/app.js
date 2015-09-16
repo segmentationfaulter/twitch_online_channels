4874996ed8964a6e4f830b66e2e76572a97005a9
segmentationfaulter <segmentationfaulter@gmail.com>
2015-09-16 14:19:59 +0500

    send an ajax request for each channel and log it to console for debugging purpose

--- js/app.js
+++ js/app.js
@@ -1,3 +1,29 @@
 $(document).ready(() => {
   'use strict';
+
+  // array of channels we are interested in
+  const channels = [
+  'freecodecamp',
+  'medrybw',
+  'storbeck',
+  'terakilobyte',
+  'habathcx',
+  'RobotCaleb',
+  'thomasballinger',
+  'noobs2ninjas',
+  'beohoff'
+];
+  channels.forEach(channel => {
+    $.ajax({
+      url : `https://api.twitch.tv/kraken/streams/${channel}`,
+      type : 'GET',
+      dataType : 'json',
+      success : success
+    });
+  });
+
+  // ajax request success callback
+  function success(response) {
+    console.log(response);
+  }
 });
